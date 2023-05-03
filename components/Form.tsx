68b735e656182851695ed6bd8d21050fe6286929
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2023-05-03 11:03:42 +0200

    request ok

--- components/Form.tsx
+++ components/Form.tsx
@@ -23,10 +23,22 @@ const Form = (): JSX.Element => {
   const [answer, setAnswer] = useState<Answer>();
   const [loading, setLoading] = useState(false);
   const [theme, setTheme] = useState('');
+  const [level, setLevel] = useState('');
+  const [numberOfQuestions, setNumberOfQuestions] = useState(0);
 
   //make a request to openAI api
 
   function openaiFetchAPI() {
+    const quizModel: Quizz = {
+      questions: [
+        {
+          id: 1,
+          text: 'Quelle est la capitale de la France ?',
+          answer: 'Paris',
+        },
+      ],
+    };
+
     setLoading(true);
     const url = 'https://api.openai.com/v1/chat/completions';
     const bearer = 'Bearer ' + process.env.NEXT_PUBLIC_OPENAI_API_KEY;
@@ -41,16 +53,22 @@ const Form = (): JSX.Element => {
         messages: [
           {
             role: 'user',
-            content: `Génère moi un quizz sur le thème de ${theme} `,
+            content: `Sur le modèle de ${quizModel}, Génère moi un qcm de ${numberOfQuestions} questions de niveau ${level} sur le thème de ${theme}.`,
           },
         ],
       }),
     })
       .then((response) => {
+        console.log(response, 'response');
+
         return response.json();
       })
       .then((data) => {
+        console.log(data, 'data');
+
         console.log(data['choices'][0]);
+        // console.log(JSON.parse(data['choices'][0].message), 'message');
+
         setAnswer(data['choices'][0].message);
         setLoading(false);
       })
@@ -62,23 +80,49 @@ const Form = (): JSX.Element => {
   return (
     <div>
       <h1>Welcome on My GPT Quizz Générator!</h1>
-      <label htmlFor=''>Thème : </label>
 
-      <input
-        type='text'
-        className='m-2 border-2 border-black rounded-md w-100 text-stone-800 px-2'
-        onChange={(e) => {
-          setTheme(e.target.value);
-        }}
-      />
-      <button
-        className='border-2 border-black px-2 py-1 bg-slate-300 br-2 hover:bg-slate-400 rounded-md'
-        onClick={() => {
-          openaiFetchAPI();
-        }}>
-        Valider
-      </button>
-      <div>{loading ? <Loader /> : answer?.content}</div>
+      <div className='flex-col'>
+        <label htmlFor=''>Thème : </label>
+
+        <input
+          type='text'
+          className='m-2 border-2 border-black rounded-md w-100 text-stone-800 px-2'
+          onChange={(e) => {
+            setTheme(e.target.value);
+          }}
+        />
+        <label>Niveau : </label>
+
+        <select
+          className='rounded-md text-stone-800 mr-2'
+          onChange={(e) => setLevel(e.target.value)}>
+          <option value=''>Choisir</option>
+          <option value='primaire'>Primaire</option>
+          <option value='collège'>Collège</option>
+          <option value='seconde'>Seconde</option>
+          <option value='première'>Première</option>
+          <option value='terminale'>Terminale</option>
+          <option value='bts'>BTS</option>
+        </select>
+
+        <label>Nombre de questions : </label>
+
+        <input
+          type='number'
+          min={3}
+          max={10}
+          onChange={(e) => setNumberOfQuestions(parseInt(e.target.value))}
+          className='m-2 border-2 border-black rounded-md w-100 text-stone-800 px-2 ml-2'
+        />
+        <button
+          className=' ml-2 border-2 border-black px-2 py-1 bg-slate-300 br-2 hover:bg-slate-400 rounded-md'
+          onClick={() => {
+            openaiFetchAPI();
+          }}>
+          Valider
+        </button>
+        <div>{loading ? <Loader /> : answer?.content}</div>
+      </div>
     </div>
   );
 };
